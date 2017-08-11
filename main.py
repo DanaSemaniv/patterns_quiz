3c1a37f27fba7adbc7202f68af16565cb5fd9040
Dana <danasemaniv@gmail.com>
2017-08-11 16:14:13 +0300

    Add game loop

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ from helpers import get_patterns, validate_input
 def print_games(games):
     print('\nEnter the number of game you want to play:\n')
 
-    for game in games:
-        print('Game {}. {}\n'.format(game, games[game].__name__.capitalize()))
+    for game_num, game in enumerate(games):
+        print('Game {}. {}\n'.format(game_num + 1,
+                                     game.__name__.capitalize()))
+
+    print('{}. Exit game'.format(len(games) + 1))
 
 if __name__ == '__main__':
     patterns = get_patterns('patterns.json')
 
-    print_games(games)
-    game = validate_input(len(games))
+    games_num = len(games) + 1
 
-    games[game](patterns)
+    while True:
+        print_games(games)
+        game = validate_input(games_num)
+        if game == games_num:
+            break
+        games[game - 1](patterns)
